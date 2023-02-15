5a79e58324e6ce296b9cda89ca6bda79074a317f
peduhvitor <pedrovitor4012@gmail.com>
2023-02-15 10:49:13 -0300

    rota de formas de pagamento adicionada

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,11 +1,13 @@
 import { useRoutes } from "react-router-dom"
 import Cart from "./pages/Cart"
 import CompleteRegister from "./pages/CompleteRegister"
+import PaymentForm from "./pages/PaymentForm"
 
 const MainRoutes = () => {
     return useRoutes([
         { path: '/cart', element: <Cart/> },
-        { path: '/step-buy/complete-register', element: <CompleteRegister/> }
+        { path: '/step-buy/complete-register', element: <CompleteRegister/> },
+        { path: '/step-buy/payment-form', element: <PaymentForm/> }
     ])
 }
 
