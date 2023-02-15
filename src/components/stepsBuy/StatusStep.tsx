ac975c58ec1acd3ac543f3d81348a61e4eae130c
peduhvitor <pedrovitor4012@gmail.com>
2023-02-15 10:56:23 -0300

    finalizando estilização do componente de Status Step

--- src/components/stepsBuy/StatusStep.tsx
+++ src/components/stepsBuy/StatusStep.tsx
@@ -7,21 +7,21 @@ const StatusStep = () => {
     
     return (
         <div className="h-min flex flex-col gap-6 max-2xl:max-w-[820px] max-2xl:w-full flex-1 p-6 bg-[#F2F3F5] rounded-[40px] text-[#6D737D] text-[18px]">
-            <div>
+            <div className="flex justify-between items-center">
                 <div className={ setStyle(state.statusStep.completeRegister).title }>Complete seu cadastro</div>
                 <div className={ `${setStyle(state.statusStep.completeRegister).img} w-6 h-6 rounded-full` }></div>
             </div>
 
             <div className="line bg-[#DADFE8]"></div>
 
-            <div>
+            <div className="flex justify-between items-center">
                 <div className={ setStyle(state.statusStep.paymentForm).title }>Complete seu cadastro</div>
                 <div className={ `${setStyle(state.statusStep.paymentForm).img} w-6 h-6 rounded-full` }></div>
             </div>
 
             <div className="line bg-[#DADFE8]"></div>
 
-            <div>
+            <div className="flex justify-between items-center">
                 <div className={ setStyle(state.statusStep.details).title }>Complete seu cadastro</div>
                 <div className={ `${setStyle(state.statusStep.details).img} w-6 h-6 rounded-full` }></div>
             </div>
