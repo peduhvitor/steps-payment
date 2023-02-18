ab8c016e6252a0c50c79988e390b883fdcb80a78
peduhvitor <pedrovitor4012@gmail.com>
2023-02-18 12:51:09 -0300

    Melhorando código do middleware

--- src/middlewares/CompleteRegisterVerify.tsx
+++ src/middlewares/CompleteRegisterVerify.tsx
@@ -1,11 +1,7 @@
 const CompleteRegisterVerify = ({children}: React.PropsWithChildren) => {
     const logged = false
-
-    return (
-        <>
-            {logged ? "Você já está logado" : children}
-        </>
-    )
+    
+    return <>{logged ? "Você já está logado" : children}</>
 }
 
 export default CompleteRegisterVerify
