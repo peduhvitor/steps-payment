c4b10e027c9f6f05209c8a122f287928dbc28d1f
peduhvitor <pedrovitor4012@gmail.com>
2023-02-21 01:22:46 -0300

    Criado o reducer de pedido

---
+++ src/reducers/order.ts
@@ -0,0 +1,68 @@
+import { reducerAction } from "../types/reducerAction"
+import { listCartType } from "./listCart"
+
+export type InitialStateType = {
+    list: listCartType[],
+    user: {
+        id: string,
+        name: string,
+        email: string,
+    },
+    address: {
+        road: string,
+        number: string,
+        neighborhood: string,
+        city: string,
+        state: string
+    },
+    paymentForm: {
+        method: string,
+        infoCard?: {
+            name: string,
+            number: string,
+            validity: string,
+            cvc: string
+        }
+    },
+    orderDetails: {
+        code: string,
+        status: string
+    }
+}
+
+export const initialState: InitialStateType = {
+    list: [],
+    user: {
+        id: '',
+        name: '',
+        email: '',
+    },
+    address: {
+        road: '',
+        number: '',
+        neighborhood: '',
+        city: '',
+        state: ''
+    },
+    paymentForm: {
+        method: ''
+    },
+    orderDetails: {
+        code: '',
+        status: ''
+    }
+}
+
+export const reducer = (state: InitialStateType, action: reducerAction) => {
+    if(action.type === 'CREATE_ORDER') {
+        return {
+            list: action.payload.list,
+            user: action.payload.user,
+            address: action.payload.address,
+            paymentForm: action.payload.paymentForm,
+            orderDetails: action.payload.orderDetails
+        }
+    }
+
+    return state
+}
