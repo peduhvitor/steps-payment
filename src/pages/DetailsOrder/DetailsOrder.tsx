4366d0f9e0f4a98e856cb6367a3d978d5efe3444
peduhvitor <pedrovitor4012@gmail.com>
2023-02-25 02:20:45 -0300

    feat: Página de detalhes do pedido exibindo dados no front corretamente

--- src/pages/DetailsOrder/DetailsOrder.tsx
+++ src/pages/DetailsOrder/DetailsOrder.tsx
@@ -11,7 +11,41 @@ const DetailsOrder = () => {
 
     useEffect(() => {
         console.log(state.order);
-    })
+    }, [])
+
+    const addressFormat = `${state.order.address.road} ${state.order.address.number}, ${state.order.address.neighborhood}, ${state.order.address.city} ${state.order.address.cep}`
+
+    const defineOrderStatus = () => {
+        if(state.order.orderDetails.status === 'preparingForDelivery') {
+            return 'Preparando para entrega'
+        }
+    }
+
+    const defineDataByPaymentForm = () => {
+        if(state.order.paymentForm.method === 'pix') {
+            return {
+                method: 'Pix',
+                title: 'Falta só mais um passo',
+                subtitle: 'Efetue o pagamento usando o QR Code abaixo em alguns segundos estaremos confirmando seu pagamento'
+            }
+        }
+
+        if(state.order.paymentForm.method === 'ticket') {
+            return {
+                method: 'Boleto',
+                title: 'Falta só mais um passo',
+                subtitle: 'Clique no seu boleto abaixo para efetuar o pagamento, e te avisaremos no email assim que confirmado'
+            }
+        }
+
+        if(state.order.paymentForm.method === 'credit-card') {
+            return {
+                method: 'Cartão de crédito',
+                title: 'Uhull! Pedido confirmado',
+                subtitle: 'Agora é só aguardar a confirmação do pagamento no seu email'
+            }
+        }
+    }
 
     return (
         <div className="flex flex-col w-full items-center my-10">
@@ -21,28 +55,28 @@ const DetailsOrder = () => {
                 <div className="flex max-2xl:flex-col max-2xl:items-center max-2xl:w-full flex-1 gap-8">
                     <div className="max-w-[820px] w-full h-min p-6 flex flex-col items-center gap-6 bg-[#F2F3F5] rounded-[40px]">
                         <div className="flex flex-col items-center gap-1">
-                            <div className="text-[24px] font-medium text-[#3F4E6E]">Uhull! Pedido confirmado</div>
-                            <div>Agora é só aguardar a confirmação do pagamento no seu email</div>
+                            <div className="text-[24px] font-medium text-[#3F4E6E]">{defineDataByPaymentForm()?.title}</div>
+                            <div className="text-center w-10/12">{defineDataByPaymentForm()?.subtitle}</div>
                         </div>
 
                         <div className="flex w-full gap-5 max-sm:flex-col-reverse">
                             <div className="flex flex-col gap-4 border-2 p-7 w-1/3 max-sm:w-full rounded-2xl">
-                                <div className="text-[18px] font-medium">Seu pedido é o #564156455</div>
+                                <div className="text-[18px] font-medium">Seu pedido é o {state.order.orderDetails.code}</div>
 
                                 <div className="flex flex-col gap-5">
                                     <div className="flex flex-col">
                                         <div className="font-medium">Endereço de entrega</div>
-                                        <div>endereço</div>
+                                        <div>{addressFormat}</div>
                                     </div>
 
                                     <div className="flex flex-col">
                                         <div className="font-medium">Forma de pagamento</div>
-                                        <div>Cartão de crédito</div>
+                                        <div>{defineDataByPaymentForm()?.method}</div>
                                     </div>
 
                                     <div className="flex flex-col">
                                         <div className="font-medium">Status do pedido</div>
-                                        <div>Preparando para entrega</div>
+                                        <div>{defineOrderStatus()}</div>
                                     </div>
                                 </div>
                             </div>
